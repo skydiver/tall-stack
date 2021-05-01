eff434c82c527669b58a1f9be9f6bdaeef3347fe
Martín M <skydiver@users.noreply.github.com>
2021-05-01 09:11:25 -0300

    installed tailwindcss

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TALL Stack</title>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-200 text-gray-800">
+    <div class="h-screen flex flex-row overflow-hidden">
+        @yield('content')
+    </div>
+    <script src="{{ mix('/js/app.js') }}"></script>
+    @stack('scripts')
+</body>
+</html>
