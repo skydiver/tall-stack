d7cd6eb5a67c15018b3b4645a0e06f1f1f88e6de
Martín M <skydiver@users.noreply.github.com>
2021-05-01 09:13:58 -0300

    installed livewire

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>TALL Stack</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <livewire:styles />
 </head>
 <body class="bg-gray-200 text-gray-800">
     <div class="h-screen flex flex-row overflow-hidden">
         @yield('content')
     </div>
+    <livewire:scripts />
     <script src="{{ mix('/js/app.js') }}"></script>
     @stack('scripts')
 </body>
